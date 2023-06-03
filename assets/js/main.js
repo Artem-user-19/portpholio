df057e37d932331f044228192c497d427aac0b85
unknown <temakot254@gmail.com>
2023-06-03 15:21:14 +0300

    port

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,35 @@
-emailjs.init('OTMtb4vbOxp7Wr4Ke')
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
 
-let from_name = document.getElementById("from_name")
-let from_email = document.getElementById("from_email")
-let from_msg = document.getElementById("message")
+mediaButton.onclick = function() {
 
-const btn = document.getElementById('button');
+    "use strict";
+
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+
+};
+
+const x = document.getElementById("x")
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+const btn = document.getElementById("button");
 
 document.getElementById('form')
     .addEventListener('submit', function(event) {
@@ -13,22 +38,14 @@ document.getElementById('form')
         btn.value = 'Sending...';
 
         const serviceID = 'default_service';
-        const templateID = 'template_v53ze08';
+        const templateID = 'template_y5lo6aj';
 
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
                 btn.value = 'Send Email';
-                btn.value = "Sent";
+                alert('Sent!');
             }, (err) => {
-                btn.value = 'Sent';
+                btn.value = 'Send Email';
                 alert(JSON.stringify(err));
             });
     });
-
-btn.addEventListener("click", () => {
-    setTimeout(() => {
-        from_name.value = ""
-        from_email.value = ""
-        from_msg.value = ""
-    }, 1500)
-})
