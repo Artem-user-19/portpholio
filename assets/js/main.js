a6e751c907f60e1749a15aba260212f8f6ef7935
unknown <temakot254@gmail.com>
2023-07-26 21:48:40 +0300

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,42 @@ document.getElementById('form')
                 alert(JSON.stringify(err));
             });
     });
+
+let mybutton = document.getElementById("myBatn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+const the_animation = document.querySelectorAll('.ani')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        } else {
+            entry.target.classList.remove('scroll-animation')
+        }
+
+    })
+}, {
+    threshold: 0.5
+});
+for (let i = 0; i < the_animation.length; i++) {
+    const elements = the_animation[i];
+
+    observer.observe(elements);
+}
